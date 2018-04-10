67f93da5b102ef77a3a4a2a560caa191393a649c
paulocpd76 <37601162+paulocpd76@users.noreply.github.com>
2018-04-10 12:03:53 +0200

    Initial commit

---
+++ app/src/main/java/pperez003/example/com/emergencyhelp/Tab3Helper.java
@@ -0,0 +1,31 @@
+package pperez003.example.com.emergencyhelp;
+
+/**
+ * Created by pperez003 on 10/04/2018.
+ */
+
+import android.support.design.widget.TabLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Tab3Helper extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab3helper, container, false);
+        return rootView;
+    }
+}
