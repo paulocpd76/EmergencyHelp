6c3cff1783899b0de1cab48cf57631b74acbaa28
paulocpd76 <37601162+paulocpd76@users.noreply.github.com>
2018-04-10 16:31:04 +0200

    created an beacon

--- app/src/main/java/pperez003/example/com/emergencyhelp/Tab1Register.java
+++ app/src/main/java/pperez003/example/com/emergencyhelp/Tab1Register.java
@@ -5,25 +5,16 @@ package pperez003.example.com.emergencyhelp;
  */
 
 
-import android.support.design.widget.TabLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-
-import android.widget.TextView;
-
+import java.util.Arrays;
+import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.BeaconTransmitter;
 
 public class Tab1Register extends  Fragment {
 
@@ -31,6 +22,19 @@ public class Tab1Register extends  Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.tab1reg, container, false);
+
+        Beacon beacon = new Beacon.Builder()
+                .setId1("2f234454-cf6d-4a0f-adf2-f4911ba9ffa6")
+                .setId2("1")
+                .setId3("2")
+                .setManufacturer(0x0118)
+                .setTxPower(-59)
+                .setDataFields(Arrays.asList(new Long[] {0l}))
+                .build();
+        BeaconParser beaconParser = new BeaconParser()
+                .setBeaconLayout("m:2-3=beac,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25");
+        BeaconTransmitter beaconTransmitter = new BeaconTransmitter(getActivity(), beaconParser);
+        beaconTransmitter.startAdvertising(beacon);
         return rootView;
     }
 }
--- app/src/main/java/pperez003/example/com/emergencyhelp/Tab3Helper.java
+++ app/src/main/java/pperez003/example/com/emergencyhelp/Tab3Helper.java
@@ -4,20 +4,9 @@ package pperez003.example.com.emergencyhelp;
  * Created by pperez003 on 10/04/2018.
  */
 
-import android.support.design.widget.TabLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
